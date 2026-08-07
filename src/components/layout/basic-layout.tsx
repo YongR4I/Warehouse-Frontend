a7de9d77164df3021b4b7cdc1d9fa80d2c0f99e1
AuzaAl <auzaganteng007@gmail.com>
2026-08-07 16:37:03 +0700

    fix: restore public images, add gitattributes for lf line endings

--- src/components/layout/basic-layout.tsx
+++ src/components/layout/basic-layout.tsx
@@ -7,9 +7,9 @@ export function BasicLayout({ children }: { children: React.ReactNode }) {
     <SidebarProvider>
       <div className="flex min-h-screen w-full">
         <AppSidebar />
-        <div className="flex flex-1 flex-col min-w-0">
+        <div className="flex min-w-0 flex-1 flex-col">
           <AppHeader />
-          <main className="flex-1 px-14 py-10 min-w-0">{children}</main>
+          <main className="min-w-0 flex-1 px-14 py-10">{children}</main>
         </div>
       </div>
     </SidebarProvider>
